5a094f377bc890105af3e5b6c499cba4d10a9401
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-01-03 14:48:41 +0300

    Добавил решение

---
+++ Test1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num > 999) || (num < 99))
+{
+ Console.WriteLine("Число не соответствует параметрам ввода");
+}
+else
+{
+ Console.WriteLine(num % 100 / 10);
+}
