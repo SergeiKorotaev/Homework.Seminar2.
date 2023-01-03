0cc36abff07de7880c0379a4ccf72d4bc7f4a03b
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-01-03 14:50:23 +0300

    Добавили решение

---
+++ Test3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num == 6) || (num == 7))
+{
+ Console.WriteLine("Выходной день");
+}
+else
+{
+ Console.WriteLine("Входным днем не является");
+}
